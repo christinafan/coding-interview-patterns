1e30b3c1c1b224213f929326e562cdd0be52af16
Christina Fan <christina.fan@berkeley.edu>
2026-02-08 20:57:01 -0800

    fix starting index of hamming weights dp solution

--- java/Bit Manipulation/HammingWeightsOfIntegersDp.java
+++ java/Bit Manipulation/HammingWeightsOfIntegersDp.java
@@ -3,7 +3,7 @@ public class HammingWeightsOfIntegersDp {
         // Base case: the number of set bits in 0 is just 0. We set dp[0] to 
         // 0 by initializing the entire DP array to 0.
         int[] dp = new int[n + 1];
-        for (int x = 0; x < n + 1; x++) {
+        for (int x = 1; x < n + 1; x++) {
             // 'dp[x]' is obtained using the result of 'dp[x >> 1]', plus 
             // the LSB of 'x'.
             dp[x] = dp[x >> 1] + (x & 1);
